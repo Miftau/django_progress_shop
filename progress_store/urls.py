b08bdd184eec8d455e4a9d8dec5543c41055e7fb
Miftau <miftaudeenb@gmail.com>
2025-01-15 01:58:46 +0100

    Fixing bugs on static

--- progress_store/urls.py
+++ progress_store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('product/<int:product_id>/review/', views.submit_review, name='submit_review'),
 ]
--- progress_store/views.py
+++ progress_store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.db.models import Avg
+from django.urls import reverse
 from .models import Product, Review
 from .forms import ReviewForm
 from progress_store import models
@@ -9,10 +10,29 @@ def home(request):
     products = Product.objects.annotate(avg_rating=Avg('reviews__rating'))
     return render(request, 'home.html', {'products':products})
 
+def about(request):
+    home_url = reverse('home')  # Ensure 'home' is the correct view name
+    return render(request, 'about.html', {'home_url': home_url})
+
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    reviews = product.reviews.all()  # Use the related_name from the ForeignKey
-    return render(request, 'product_detail.html', {'product': product, 'reviews': reviews})
+    reviews = Review.objects.filter(product=product)
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.save()
+            return redirect('product_detail', product_id=product.id)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'product_detail.html', {
+        'product': product,
+        'form': form,
+        'reviews': reviews,
+    })
 
 def submit_review(request, product_id):
     product = get_object_or_404(Product, id=product_id)
